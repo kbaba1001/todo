5010718820b553bf4dc61a07cb88670f15539a7b
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-09-20 17:28:26 +0900

    migration using ragtime

--- dev/src/user.clj
+++ dev/src/user.clj
@@ -1,4 +1,6 @@
-(ns user)
+(ns user
+  (:require [ragtime.jdbc :as jdbc]
+            [ragtime.repl :as repl]))
 
 (defn dev
   "Load and switch to the 'dev' namespace."
@@ -6,3 +8,13 @@
   (require 'dev)
   (in-ns 'dev)
   :loaded)
+
+(defn- migration-config []
+  {:datastore  (jdbc/sql-database {:connection-uri "jdbc:postgresql://db:5432/todo-development?user=postgres&password="})
+   :migrations (jdbc/load-resources "migrations")})
+
+(defn migrate []
+  (repl/migrate (migration-config)))
+
+(defn rollback []
+  (repl/rollback (migration-config)))
