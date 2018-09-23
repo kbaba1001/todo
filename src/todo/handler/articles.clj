268a137dbc30f5c15c4b57f3751415de49546f56
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-09-23 14:41:56 +0900

    DBの値を読むhandlerを作成

--- src/todo/handler/articles.clj
+++ src/todo/handler/articles.clj
@@ -1,11 +1,9 @@
 (ns todo.handler.articles
   (:require [ataraxy.response :as response]
-            [integrant.core :as ig]))
+            [integrant.core :as ig]
+            [clojure.java.jdbc :as jdbc]))
 
-; [clojure.java.jdbc :as jdbc]
-; TODO ["SELECT * FROM articles"] のクエリの結果を response/ok で返す
 (defmethod ig/init-key ::index [_ {:keys [db]}]
   (fn [request]
-    [::response/ok "OK!!"]))
-
-; [{"id":1,"content":"hello"}]
+    (let [articles (jdbc/query (:spec db) ["SELECT * FROM articles"])]
+      [::response/ok articles])))
