2df51d01949589ac5725860a69db684e0bbfca66
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-09-27 18:25:33 +0900

    add boundary test

---
+++ test/todo/boundary/articles_test.clj
@@ -0,0 +1,19 @@
+(ns todo.boundary.articles-test
+  (:require duct.database.sql
+            [todo.test-utils :as u]
+            [clojure.test :as t]
+            [clojure.java.jdbc :as jdbc]
+            [todo.boundary.articles :as articles]))
+
+(t/deftest boundary-artilces-test
+  (do
+    (t/testing "create article"
+      (let [article-id (articles/create-article u/db {:content "foo"})]
+        (t/is (int? article-id))))
+
+    (t/testing "index artilces"
+      (let [results (articles/index-articles u/db)]
+        (t/is (= 1 (count results)))
+        (t/is (= "foo" (:content (first results))))))
+
+    (u/drop-all-tables)))
