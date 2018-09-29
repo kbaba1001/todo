2dc513b26b8e6086454bcd3362d4cc97fae1e2d0
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-09-29 23:01:31 +0900

    define users/signin handler

--- src/todo/handler/users.clj
+++ src/todo/handler/users.clj
@@ -1,6 +1,7 @@
 (ns todo.handler.users
   (:require [ataraxy.response :as response]
             [integrant.core :as ig]
+            [buddy.sign.jwt :as jwt]
             [todo.boundary.users :as users]))
 
 (defmethod ig/init-key ::create [_ {:keys [db]}]
@@ -8,6 +9,11 @@
     (let [user-id (users/create-user db body-params)]
       [::response/created (str "/users/" user-id)])))
 
-(defmethod ig/init-key ::signin [_ {:keys [db]}]
-  (fn [request]
-    [::response/ok "OK!!"]))
+(defmethod ig/init-key ::signin [_ {:keys [db jwt-secret]}]
+  (fn [{:keys [body-params]}]
+    (letfn [(with-token [user]
+              (->> (jwt/sign (select-keys user [:email]) jwt-secret)
+                   (assoc user :token)))]
+      (if-let [user (users/authenticate-user db body-params)]
+        [::response/ok {:user (with-token user)}]
+        [::response/forbidden "Not authorized"]))))
