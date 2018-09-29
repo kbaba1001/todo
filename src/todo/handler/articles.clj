50a62aee964fb3643d49d4abe96ee50e4a43a9ee
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-09-29 23:10:53 +0900

    require authentication path

--- src/todo/handler/articles.clj
+++ src/todo/handler/articles.clj
@@ -1,6 +1,7 @@
 (ns todo.handler.articles
   (:require [ataraxy.response :as response]
             [integrant.core :as ig]
+            [buddy.auth :as buddy]
             [todo.boundary.articles :as articles]))
 
 (defmethod ig/init-key ::index [_ {:keys [db]}]
@@ -9,6 +10,8 @@
       [::response/ok articles])))
 
 (defmethod ig/init-key ::create [_ {:keys [db]}]
-  (fn [{:keys [body-params]}]
-    (let [article-id (articles/create-article db body-params)]
-      [::response/created (str "/articles/" article-id)])))
+  (fn [{:keys [body-params] :as request}]
+    (if-not (buddy/authenticated? request)
+      [::response/unauthorized "invalid token"]
+      (let [article-id (articles/create-article db body-params)]
+        [::response/created (str "/articles/" article-id)]))))
