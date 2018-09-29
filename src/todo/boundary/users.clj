06d6fb053d1a825cdbf0e8992dbb27c713b53afc
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-09-29 22:00:45 +0900

    define todo.boundary.users/create-user

--- src/todo/boundary/users.clj
+++ src/todo/boundary/users.clj
@@ -1,8 +1,15 @@
 (ns todo.boundary.users
   (:require duct.database.sql
-            [clojure.java.jdbc :as jdbc]))
+            [clojure.java.jdbc :as jdbc]
+            [buddy.hashers :as hashers]))
 
-(defprotocol Users)
+(defprotocol Users
+  (create-user [db params]))
 
 (extend-protocol Users
-  duct.database.sql.Boundary)
+  duct.database.sql.Boundary
+
+  (create-user [{:keys [spec]} {:keys [email password]}]
+    (let [password-digest (hashers/derive password)
+          result (jdbc/insert! spec :users {:email email :password-digest password-digest})]
+      (-> result first :id))))
