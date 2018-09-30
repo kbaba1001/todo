bf48c99a97ece1726bdd4b2e2ce0e53ae9e93a31
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-09-30 22:14:00 +0900

    unique email validation

--- src/todo/handler/users.clj
+++ src/todo/handler/users.clj
@@ -5,13 +5,17 @@
             [struct.core :as st]
             [todo.boundary.users :as users]))
 
-(defn- create-form-schema []
-  {:email [st/required st/email]
-   :password [st/required st/string [st/min-count 8] [st/max-count 100]]})
+(defn- create-form-schema [db]
+  (let [unique-email {:message "already used"
+                      :optional true
+                      :validate (fn [v db]
+                                  (nil? (users/find-user-by-email db v)))}]
+    {:email [st/required st/email [unique-email db]]
+     :password [st/required st/string [st/min-count 8] [st/max-count 100]]}))
 
 (defmethod ig/init-key ::create [_ {:keys [db]}]
   (fn [{:keys [body-params]}]
-    (if-let [errors (first (st/validate body-params (create-form-schema)))]
+    (if-let [errors (first (st/validate body-params (create-form-schema db)))]
       [::response/bad-request errors]
       (let [user-id (users/create-user db body-params)]
         [::response/created (str "/users/" user-id)]))))
