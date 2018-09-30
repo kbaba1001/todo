b6ce36769c121250d75b87c5e7bc602f24a393e2
kbaba1001 <kbaba1001@users.noreply.github.com>
2018-09-30 16:22:42 +0900

    add db debug methods to dev.clj

--- dev/src/dev.clj
+++ dev/src/dev.clj
@@ -6,6 +6,9 @@
             [clojure.java.io :as io]
             [duct.core :as duct]
             [duct.core.repl :as duct-repl]
+            [duct.database.sql :refer :all]
+            [duct.core.env :as e]
+            [clojure.java.jdbc :as jdbc]
             [eftest.runner :as eftest]
             [integrant.core :as ig]
             [integrant.repl :refer [clear halt go init prep reset]]
@@ -18,6 +21,14 @@
 
 (defn test []
   (eftest/run-tests (eftest/find-tests "test") {:capture-output? false :multithread? false}))
+
+(def db-spec
+  {:connection (jdbc/get-connection {:connection-uri (e/env "DEV_DB_URL")})})
+
+(def db (->Boundary db-spec))
+
+(defn q [sql]
+  (jdbc/query db-spec sql))
 
 (clojure.tools.namespace.repl/set-refresh-dirs "dev/src" "src" "test")
 
